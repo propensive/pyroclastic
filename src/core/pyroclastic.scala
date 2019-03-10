ea797c5cbfe286501520911639a6d01ec8507806
Jon Pretty <jon.pretty@propensive.com>
2019-03-10 17:22:00 +0000

    Support for propagating/providing two values

--- src/core/pyroclastic.scala
+++ src/core/pyroclastic.scala
@@ -25,7 +25,7 @@ class Domain[M[_]]()(implicit monadic: Monadic[M]) {
     type Type = T
     def apply()(implicit env: Env[this.type]): T = env.fields(this).asInstanceOf[T]
     def of(mkField: => T): Flow[BaseKey, this.type] =
-      new Flow(this, { in => in.updated(this, monadic.point(mkField)) })
+      new Flow({ in => in.updated(this, monadic.point(mkField)) })
   }
 
   def given[In <: BaseKey](xs: Need[In]*): GivenFields[In] =
@@ -33,27 +33,39 @@ class Domain[M[_]]()(implicit monadic: Monadic[M]) {
 
   class GivenFields[In <: BaseKey](keys: Seq[BaseKey]) {
     def propagate(key: BaseKey)(action: Env[_ <: In] => M[key.Type]): Flow[In, In with key.type] =
-      new Flow(key, { in =>
-        in.updated(key, keys.map { k => in(k).map(k -> _) }.sequence.flatMap { seq => action(Env(seq.toMap)).asInstanceOf[M[Any]] })
+      new Flow({ in =>
+        val input = keys.map { k => in(k).map(k -> _) }.sequence
+        val result = input.flatMap { seq => action(Env(seq.toMap)).asInstanceOf[M[Any]] }
+        in.updated(key, result)
+      })
+    
+    def propagate(key1: BaseKey, key2: BaseKey)(action: Env[_ <: In] => (M[key1.Type], M[key2.Type])): Flow[In, In with key1.type with key2.type] =
+      new Flow({ in =>
+        val input = keys.map { k => in(k).map(k -> _) }.sequence
+        val result = input.flatMap { seq =>
+          val (a, b) = action(Env(seq.toMap))
+          a.flatMap { a => b.map((a, _)) }
+        }.asInstanceOf[M[(Any, Any)]]
+        
+        in.updated(key1, result.map(_._1)).updated(key2, result.map(_._2))
       })
     
     def provide(key: BaseKey)(action: Env[_ <: In] => key.Type): Flow[In, In with key.type] =
-      new Flow(key, { in =>
-        in.updated(key, keys.map { key => in(key).map(key -> _) }.sequence.flatMap { seq => monadic.point(action(Env(seq.toMap))).asInstanceOf[M[Any]] })
-      })
+      propagate(key)(action.andThen(monadic.point(_)))
+    
+    def provide(key1: BaseKey, key2: BaseKey)(action: Env[_ <: In] => (key1.Type, key2.Type)): Flow[In, In with key1.type with key2.type] =
+      propagate(key1, key2)(action.andThen { case (a, b) => (monadic.point(a), monadic.point(b)) })
   }
 
   case class Env[+In <: BaseKey](fields: Map[BaseKey, Any])
 
-  case class Flow[In <: BaseKey, Out <: BaseKey]
-                 (key: BaseKey,
-                  flow: Map[BaseKey, M[Any]] => Map[BaseKey, M[Any]]) {
+  case class Flow[In <: BaseKey, Out <: BaseKey](flow: Map[BaseKey, M[Any]] => Map[BaseKey, M[Any]]) {
 
     def >>>[In2 <: BaseKey, Out2 <: BaseKey]
            (next: Flow[In2, Out2])
            (implicit ev: Out <:< In2, flat: Flat[In with Out with Out2])
            : Flow[In, flat.Type] =
-      new Flow(next.key, { in => next.flow(flow(in)) })
+      new Flow({ in => next.flow(flow(in)) })
 
     def apply[T](key: Field[T])(implicit ev: Out <:< key.type, ev2: BaseKey =:= In): M[T] =
       flow(Map())(key).asInstanceOf[M[T]]
