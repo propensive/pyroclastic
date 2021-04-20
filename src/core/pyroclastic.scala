979c76e27a8e16a1962aa8af110a6c6c1c0240c6
Jon Pretty <jon.pretty@propensive.com>
2021-04-20 16:39:14 +0200

    Work in progress

--- src/core/pyroclastic.scala
+++ src/core/pyroclastic.scala
@@ -1,69 +1,86 @@
 package pyroclastic
 
-import mercator._
+import scala.concurrent.*
 
-import scala.annotation.unchecked.{uncheckedVariance => uv}
-import language.implicitConversions, language.higherKinds
+given ExecutionContext = ExecutionContext.global
 
-sealed trait BaseKey { type Type }
+case class Env[-E](elements: Map[Point[?], Future[Any]])
 
-object Flat { implicit def flat[T]: Flat[T] = null }
-class Flat[T] { type Type = T }
+type Context = Map[Point[_], Future[Any]]
 
-object Need { implicit def toNeed[T <: BaseKey](t: T): Need[t.type] = Need(t) }
-case class Need[-T <: BaseKey](field: T @uv)
+case class Flow[In <: Singleton, Out <: Singleton](flow: Context => Context):
+  def >>>[In2, Out2](next: Flow[In2, Out2])(using Out <:< In2): Flow[In, In & Out & Out2] =
+    Flow({ in => next.flow(flow(in)) })
 
-object Domain {
-  def over[M[_]: Monadic]: Domain[M] = new Domain()(implicitly[Monadic[M]])
-}
+case class Point[T]():
+  def apply(value: T): Flow[Singleton, this.type] = Flow(_.updated(this, Future(value)))
+  def apply()(using Env[this.type]) = summon[Env[this.type]].elements(this).asInstanceOf[T]
 
-class Domain[M[_]]()(implicit monadic: Monadic[M]) {
+  def &[S](that: Point[S]): Point[this.type | that.type] = Point()
 
-  object Field { def apply[T](): Field[T] = new Field() }
+  def update[S](newValue: T)(using Env[S]): Env[S & T] = Env(summon[Env[S]].elements.updated(this, Future(newValue)))
 
-  class Field[T]() extends BaseKey {
-    type Type = T
-    def apply()(implicit env: Env[this.type]): T = env.fields(this).asInstanceOf[T]
-    def of(mkField: => T): Flow[BaseKey, this.type] =
-      new Flow({ in => in.updated(this, monadic.point(mkField)) })
-  }
+  def propagate[S](fn: Env[T] ?=> Env[S]): Flow[T, S & T] = ???
 
-  def given[In <: BaseKey](xs: Need[In]*): GivenFields[In] =
-    new GivenFields(xs.map(_.field))
 
-  class GivenFields[In <: BaseKey](keys: Seq[BaseKey]) {
-    def propagate(key: BaseKey)(action: Env[_ <: In] => M[key.Type]): Flow[In, In with key.type] =
-      new Flow({ in =>
-        val input = keys.map { k => in(k).map(k -> _) }.sequence
-        val result = input.flatMap { seq => action(Env(seq.toMap)).asInstanceOf[M[Any]] }
-        in.updated(key, result)
-      })
-    
-    def propagate(key1: BaseKey, key2: BaseKey)(action: Env[_ <: In] => M[(key1.Type, key2.Type)]): Flow[In, In with key1.type with key2.type] =
-      new Flow({ in =>
-        val input = keys.map { k => in(k).map(k -> _) }.sequence
-        val result = input.flatMap { seq => action(Env(seq.toMap)).asInstanceOf[M[(Any, Any)]] }
-        in.updated(key1, result.map(_._1)).updated(key2, result.map(_._2))
-      })
-    
-    def provide(key: BaseKey)(action: Env[_ <: In] => key.Type): Flow[In, In with key.type] =
-      propagate(key)(action.andThen(monadic.point(_)))
-    
-    def provide(key1: BaseKey, key2: BaseKey)(action: Env[_ <: In] => (key1.Type, key2.Type)): Flow[In, In with key1.type with key2.type] =
-      propagate(key1, key2)(action.andThen { case (a, b) => monadic.point((a, b)) })
-  }
+// import scala.annotation.unchecked.{uncheckedVariance => uv}
 
-  case class Env[+In <: BaseKey](fields: Map[BaseKey, Any])
+// given ExecutionContext = scala.concurrent.ExecutionContext.global
 
-  case class Flow[In <: BaseKey, Out <: BaseKey](flow: Map[BaseKey, M[Any]] => Map[BaseKey, M[Any]]) {
+// import scala.concurrent.*
 
-    def >>>[In2 <: BaseKey, Out2 <: BaseKey]
-           (next: Flow[In2, Out2])
-           (implicit ev: Out <:< In2, flat: Flat[In with Out with Out2])
-           : Flow[In, flat.Type] =
-      new Flow({ in => next.flow(flow(in)) })
+// sealed trait BaseKey { type Type }
 
-    def apply[T](key: Field[T])(implicit ev: Out <:< key.type, ev2: BaseKey =:= In): M[T] =
-      flow(Map())(key).asInstanceOf[M[T]]
-  }
-}
+// object Flat { implicit def flat[T]: Flat[T] = null }
+// class Flat[T] { type Type = T }
+
+// object Need { implicit def toNeed[T <: BaseKey](t: T): Need[t.type] = Need(t) }
+// case class Need[-T <: BaseKey](field: T @uv)
+
+// object Field { def apply[T](): Field[T] = new Field() }
+
+// class Field[T]() extends BaseKey {
+//   type Type = T
+//   def apply()(implicit env: Env[this.type]): T = env.fields(this).asInstanceOf[T]
+//   def of(mkField: => T): Flow[BaseKey, this.type] =
+//     new Flow({ in => in.updated(this, Future(mkField)) })
+// }
+
+// def on[In <: BaseKey](xs: Need[In]*): GivenFields[In] =
+//   new GivenFields(xs.map(_.field))
+
+// class GivenFields[In <: BaseKey](keys: Seq[BaseKey]) {
+//   def propagate(key: BaseKey)(action: Env[_ <: In] => Future[key.Type]): Flow[In, In with key.type] =
+//     new Flow({ in =>
+//       val input = Future.sequence(keys.map { k => in(k).map(k -> _) })
+//       val result = input.flatMap { seq => action(Env(seq.toMap)).asInstanceOf[Future[Any]] }
+//       in.updated(key, result)
+//     })
+  
+//   def propagate(key1: BaseKey, key2: BaseKey)(action: Env[_ <: In] => Future[(key1.Type, key2.Type)]): Flow[In, In with key1.type with key2.type] =
+//     new Flow({ in =>
+//       val input = Future.sequence(keys.map { k => in(k).map(k -> _) })
+//       val result = input.flatMap { seq => action(Env(seq.toMap)).asInstanceOf[Future[(Any, Any)]] }
+//       in.updated(key1, result.map(_._1)).updated(key2, result.map(_._2))
+//     })
+  
+//   def provide(key: BaseKey)(action: Env[_ <: In] => key.Type): Flow[In, In with key.type] =
+//     propagate(key)(action.andThen(Future(_)))
+  
+//   def provide(key1: BaseKey, key2: BaseKey)(action: Env[_ <: In] => (key1.Type, key2.Type)): Flow[In, In with key1.type with key2.type] =
+//     propagate(key1, key2)(action.andThen { case (a, b) => Future((a, b)) })
+// }
+
+// case class Env[+In <: BaseKey](fields: Map[BaseKey, Any])
+
+// case class Flow[In <: BaseKey, Out <: BaseKey](flow: Map[BaseKey, Future[Any]] => Map[BaseKey, Future[Any]]) {
+
+//   def >>>[In2 <: BaseKey, Out2 <: BaseKey]
+//           (next: Flow[In2, Out2])
+//           (implicit ev: Out <:< In2, flat: Flat[In with Out with Out2])
+//           : Flow[In, flat.Type] =
+//     new Flow({ in => next.flow(flow(in)) })
+
+//   def apply[T](key: Field[T])(implicit ev: Out <:< key.type, ev2: BaseKey =:= In): Future[T] =
+//     flow(Map())(key).asInstanceOf[Future[T]]
+// }
--- src/example/test.scala
+++ src/example/test.scala
@@ -1,19 +1,16 @@
-package pyroclastic.tests
+package pyroclastic
 
-import mercator._
-import language.implicitConversions
+import probably.*
 
-object Test extends App {
+import scala.util.*
 
-  import pyroclastic._
-  import scala.util._
-
-  val domain = Domain.over[Try]
-  import domain._
+object Test extends Suite("Pyroclastic tests"):
 
   case class Milk()
   case class Tea()
-  case class Water(hot: Boolean) { def boil(): Try[Water] = Success(Water(true)) }
+  case class Water(hot: Boolean):
+    def boil(): Water = Water(true)
+
   case class Kettle()
   case class Coffee()
   case class Teabag()
@@ -26,44 +23,51 @@ object Test extends App {
       if(math.random < 0.9) Success(Cup(water, milk, other)) else Failure(Spillage())
   }
   
-  val milk = Field[Milk]()
-  val hotWater = Field[Water]()
-  val coldWater = Field[Water]()
-  val kettle = Field[Kettle]()
-  val teabag = Field[Tea]()
-  val tea = Field[Cup[Tea]]()
-  val groundCoffee = Field[Coffee]()
-  val coffee = Field[Cup[Coffee]]()
+  val milk = Point[Milk]()
+  val tea = Point[Tea]()
+  val water = Point[Water]()
 
-  val oldTeabag = Field[Teabag]()
+  def run(using Runner): Unit =
+    
+    val hotWater = Point[Water]()
+    val coldWater = Point[Water]()
+    val kettle = Point[Kettle]()
+    val teabag = Point[Tea]()
+    val tea = Point[Cup[Tea]]()
+    val groundCoffee = Point[Coffee]()
+    val coffee = Point[Cup[Coffee]]()
 
-  val getKettle = kettle.of(Kettle())
-  val getWater = coldWater.of(Water(false))
-  val grindCoffee = groundCoffee.of(Coffee())
-  val buyMilk = milk.of(Milk())
-  val buyTeabag = teabag.of(Tea())
+    val oldTeabag = Point[Teabag]()
 
-  val boilWater = given(coldWater, kettle).propagate(hotWater) { implicit env => println("boiling..."); coldWater().boil() }
+    val getKettle = kettle(Kettle())
+    val getWater = coldWater(Water(false))
+    val grindCoffee = groundCoffee(Coffee())
+    val buyMilk = milk(Milk())
+    val buyTeabag = teabag(Tea())
+
+    val boilWater: Flow[coldWater.type & kettle.type, coldWater.type & kettle.type & hotWater.type] = (coldWater & kettle).propagate {
+      println("boiling...")
+      hotWater() = coldWater().boil()
+    }
   
-  val pourTea = given(milk, hotWater, teabag).propagate(tea, oldTeabag) { implicit env =>
-    Cup.pour(hotWater(), milk(), teabag()).map((_, Teabag()))
-  }
+  // val pourTea = on(milk, hotWater, teabag).propagate(tea, oldTeabag) {
+  //   Cup.pour(hotWater(), milk(), teabag()).map((_, Teabag()))
+  // }
 
-  val pourCoffee = given(milk, hotWater, groundCoffee).propagate(coffee) { implicit env =>
-    Cup.pour(hotWater(), milk(), groundCoffee())
-  }
+  // val pourCoffee = on(milk, hotWater, groundCoffee).propagate(coffee) {
+  //   Cup.pour(hotWater(), milk(), groundCoffee())
+  // }
 
-  val preparation = getKettle >>> getWater >>> buyMilk >>> boilWater 
+  // val preparation = getKettle >>> getWater >>> buyMilk >>> boilWater 
 
-  val teaRound = buyTeabag >>> preparation >>> boilWater >>> pourTea
-  val coffeeRound = grindCoffee >>> preparation >>> pourCoffee
+  // val teaRound = buyTeabag >>> preparation >>> boilWater >>> pourTea
+  // val coffeeRound = grindCoffee >>> preparation >>> pourCoffee
 
-  val myTea = teaRound(tea)
-  val myTeabag = teaRound(teabag)
-  val myCoffee: Try[Cup[Coffee]] = coffeeRound(coffee)
+  // val myTea = teaRound(tea)
+  // val myTeabag = teaRound(teabag)
+  // val myCoffee: Try[Cup[Coffee]] = coffeeRound(coffee)
 
-  println(myTea)
-  println(myTeabag)
-  println(myCoffee)
-  sys.exit(1)
-}
+  // println(myTea)
+  // println(myTeabag)
+  // println(myCoffee)
+  // sys.exit(1)
